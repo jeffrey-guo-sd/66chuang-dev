f1f0182d236698061cd6589573892cc577f98171
jguo@66chuang.com <guo.jeffrey.github@gmail.com>
2015-05-05 20:50:12 -0700

    chinese version of constants

---
+++ website/sites/all/includes/data/chuang_constant_cn.php
@@ -0,0 +1,55 @@
+<?php 
+
+// Start up profile company constant
+
+define('STARTUPCOMPANYFORM','Start Up Profile Company Form');
+define('COMPANYPROFILETITLE','Create a Startup Profile');
+define('COMPANYBUTTON','公司');
+define('TEAMBUTTON','团队');
+define('COMPANYINFOTEXT','Company Info');
+define('COMPANYNAME','公司名称');
+define('COMPANYLOGO','Logo');
+define('HIGHCONCEPTPITCH','High-Concept Pitch');
+define('COMPANYWEBSITE','Company Website');
+define('PRODUCT','Product');
+define('LOCATION','Location');
+define('MARKETS','Markets');
+define('WHYAREYOUHERE','Why are you here?');
+define('FUNDRAISING','Fundraising');
+define("FUNDRAISINGDESCRIPTION","Tell us about your current round. We'll show you fundraising options when you are ready to publish your profile.");
+define('RAISING','Raising');
+define('DEBTEQUITYSAFE','Debt, Equity, SAFE');
+define('PREMONEYVALUATION','Pre-Money Valuation');
+define('PREVIOUSFUNDING','Previous Funding');
+define('BROWSEVALUATIONS','Browse valuations');
+define('COMPANYTERMS','Terms :');
+define('COMPANYTERMSSOMEREMINDER','Some reminders from the <a class="anchorBlueLink">Terms of Service:</a>');
+define("COMPANYTERMSFIRSTTEXT","You're responsible for doing diligence on the investors and verifying they're accredited");
+define('COMPANYTERMSSECONDTEXT','You indemnify AngelList and understand the risks of raising money');
+define('COMPANYTERMSTHIRDTEXT','You will use an attorney and comply with securities laws');
+define('COMPANYRECRUITING','Recruiting');
+define('COMPANYRECRUITINGDESCRIPTION','A new job matching service. No recruiters. High­quality talent. Fun.');
+define('APPLYINGINCUBATOR','Applying to an incubator');
+define('APPLYINGINCUBATORDESCRIPTION','Apply to PandoMedia, Maven Ventures, MuckerLab, and more with your AngelList profile.');
+define('TERMSOFSERVICEDESCRIPTION','Terms of Service');
+define('COMPANYTERMSOFSERVICE','I have read and accepted the <a class="anchorBlueLink">Terms of Service</a>');
+define('COMPANYSUBMITBUTTON','Continue');
+define('SELECT','--- Select ---');
+define('EQUITY','Equity');
+define('CONVERTIBLEDEBT','Convertible Debt');
+define('SAFE','SAFE');
+define('FILEEXTENSION','Please upload images with extensions "jpg", "jpeg" , "gif"  or "png".');
+define('JPG','jpg');
+define('JPEG','jpeg');
+define('GIF','gif');
+define('PNG','png');
+define('COMPANYPROFILESUCCESSMESSAGE','Company profile has been created successfully.');
+
+
+
+
+
+
+
+
+?>
